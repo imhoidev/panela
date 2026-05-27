2c1ac4bcd59320fc7517e7b1614617b6db3b9e5c
ØRYK <129035588+oryk-xxx@users.noreply.github.com>
2026-05-27 20:40:39 -0300

    12w

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,5 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: {
-    preset: "vercel",
-    output: { dir: ".vercel/output" },
-  },
+  nitro: { preset: "vercel" },
 });
